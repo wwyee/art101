0dd1b12c96910311489878f51323a3cc2dc4c64f
wwyee <wwong32@ucsc.edu>
2025-04-21 14:33:08 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,50 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Four-in-a-Row
+// An implementation of the game
+// Marisa Wong <wwong32@ucsc.edu>
+// April 2025
 
-// Constants
+/*
+  Print an empty 6-row by 7-column grid with empty values
+  Set up two players to “red” and “yellow”
+  Decide which player goes first.
+  */
 
-// Functions
+// Show the current grid to players after every move.
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//Promt current player to choose a column (0 to 6).
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+/*
+  * If the chosen column is full:
+  *   Print "Column full, choose another.”
+  */
 
-// let's get this party started
-main();
+/*
+  * Place the player's token in the lowest available row in that column.
+  */
+
+/* Check for a Win.
+  * If the board is full
+      print "Tie"
+    
+    Else if 4 matching tokens = true:
+      Horizontally
+      Vertically
+      Diagonally (both directions)
+
+        Print "Winner is CurrentPlayer!"
+
+    Else
+      continue to next step
+*/
+
+/* Switch Turns
+  * Change to the other player.
+  * Repeat step 2-7 until there is a winner.
+*/
+
+
+/* End the Game
+  * If there’s a winner, display the winner and end the game.
+  * If it’s a tie, display a tie message and end the game.
+  * Offer option to restart or exit.
+*/
