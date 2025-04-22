24329f716d05c988b50d6b1a4ffcf9ac97bae0bd
wwyee <wwong32@ucsc.edu>
2025-04-21 20:48:26 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -4,26 +4,26 @@
 // April 2025
 
 /*
-  Print an empty 6-row by 7-column grid with empty values
-  Set up two players to “red” and “yellow”
-  Decide which player goes first.
-  */
+ * Print an empty 6-row by 7-column grid with empty values
+ * Set up two players to “red” and “yellow”
+ * Decide which player goes first.
+ */
 
 // Show the current grid to players after every move.
 
 //Promt current player to choose a column (0 to 6).
 
 /*
-  * If the chosen column is full:
-  *   Print "Column full, choose another.”
-  */
+ * If the chosen column is full:
+ *   Print "Column full, choose another.”
+ */
 
 /*
-  * Place the player's token in the lowest available row in that column.
-  */
+ * Place the player's token in the lowest available row in that column.
+ */
 
 /* Check for a Win.
-  * If the board is full
+ * If the board is full
       print "Tie"
     
     Else if 4 matching tokens = true:
@@ -35,16 +35,16 @@
 
     Else
       continue to next step
-*/
+ */
 
 /* Switch Turns
-  * Change to the other player.
-  * Repeat step 2-7 until there is a winner.
-*/
+ * Change to the other player.
+ * Repeat step 2-7 until there is a winner.
+ */
 
 
 /* End the Game
-  * If there’s a winner, display the winner and end the game.
-  * If it’s a tie, display a tie message and end the game.
-  * Offer option to restart or exit.
-*/
+ * If there’s a winner, display the winner and end the game.
+ * If it’s a tie, display a tie message and end the game.
+ * Offer option to restart or exit.
+ */
