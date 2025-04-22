48e93cefd524512120a5937871e1648c83de72b1
wwyee <wwong32@ucsc.edu>
2025-04-21 21:08:09 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,49 +1,36 @@
-// Four-in-a-Row
-// An implementation of the game
-// Marisa Wong <wwong32@ucsc.edu>
-// April 2025
-
-/*
- * Print an empty 6-row by 7-column grid with empty values
+/**
+ * Four-in-a-Row
+ * 
+ * An implementation of the game.
+ * @author Marisa Wong <wwong32@ucsc.edu>
+ * April 2025
+ * 
  * Set up two players to “red” and “yellow”
  * Decide which player goes first.
- */
-
-// Show the current grid to players after every move.
-
-//Promt current player to choose a column (0 to 6).
-
-/*
+ * 
+ * Show the current grid to players after every move.
+ * Promt current player to choose a column (0 to 6).
+ * 
  * If the chosen column is full:
- *   Print "Column full, choose another.”
- */
-
-/*
+ *  Print "Column full, choose another.”
+ * 
+ * 
  * Place the player's token in the lowest available row in that column.
- */
-
-/* Check for a Win.
+ * 
+ * Check for a Win.
  * If the board is full
-      print "Tie"
-    
-    Else if 4 matching tokens = true:
-      Horizontally
-      Vertically
-      Diagonally (both directions)
-
-        Print "Winner is CurrentPlayer!"
-
-    Else
-      continue to next step
- */
-
-/* Switch Turns
- * Change to the other player.
- * Repeat step 2-7 until there is a winner.
- */
-
-
-/* End the Game
+ *  print "Tie"
+ * 
+ * Else if 4 matching tokens = true:
+ *    Horizontally
+ *    Vertically
+ *    Diagonally (both directions)
+ *      Print "Winner is CurrentPlayer!"
+ * 
+ * Else
+ *  continue to next step
+ * 
+ * End the Game
  * If there’s a winner, display the winner and end the game.
  * If it’s a tie, display a tie message and end the game.
  * Offer option to restart or exit.
