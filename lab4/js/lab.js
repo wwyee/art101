7da755162c1318c316fcde8e824d6a26292481ea
wwyee <wwong32@ucsc.edu>
2025-04-21 21:26:45 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -5,32 +5,34 @@
  * @author Marisa Wong <wwong32@ucsc.edu>
  * April 2025
  * 
+ * ---
+ * 
  * Set up two players to “red” and “yellow”
  * Decide which player goes first.
  * 
  * Show the current grid to players after every move.
- * Promt current player to choose a column (0 to 6).
+ * Prompt current player to choose a column (0 to 6).
  * 
  * If the chosen column is full:
  *  Print "Column full, choose another.”
  * 
- * 
  * Place the player's token in the lowest available row in that column.
  * 
  * Check for a Win.
- * If the board is full
- *  print "Tie"
+ * check if there's any "connected" four:
+ * - Horizontally
+ * - Vertically
+ * - Diagonally (both directions)
+ * If the board does not have 4 matching tokens, continue to the next step.
  * 
- * Else if 4 matching tokens = true:
- *    Horizontally
- *    Vertically
- *    Diagonally (both directions)
- *      Print "Winner is CurrentPlayer!"
+ * Check for a tie
+ * if the board is not full, continue to next step.
+ * if the board is full and there is no winner, declare a tie.
  * 
- * Else
- *  continue to next step
+ * Switch turns.
+ * change to the other player.
+ * repeat line 13 - 30 until there is a winner.
  * 
- * End the Game
  * If there’s a winner, display the winner and end the game.
  * If it’s a tie, display a tie message and end the game.
  * Offer option to restart or exit.
