c827aa32042749e96fbb4b2f53e1eda7b547b5c4
wwyee <wwong32@ucsc.edu>
2025-04-24 17:55:07 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Marisa Wong
+// Date: 24 April 2025
 
-// Constants
+// Define Variables
+make = "Tesla";
+model = "X";
+color = "White";
+year: 2016;
 
-// Functions
+// calculate
+age = 2025 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
