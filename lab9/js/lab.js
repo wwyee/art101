9c34fa2afe2e5a7c19073ad8bc55b08d3b839bf0
wwyee <114123370+wwyee@users.noreply.github.com>
2025-05-12 14:33:05 -0700

    lab9

---
+++ lab9/js/lab.js
@@ -0,0 +1,13 @@
+// index.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+//Requirements: jQuery must be loaded for this script to work.
+// Author: Marisa Wong
+// Date: 12 May 2025
+
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
