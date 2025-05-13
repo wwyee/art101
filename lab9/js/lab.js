fb172c326574ea59875abad3735f853b5d4265ef
wwong32 <wwong32@ss1-mac213.lt.ucsc.edu>
2025-05-12 18:47:38 -0700

    lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -4,10 +4,24 @@
 // Date: 12 May 2025
 
 // add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$(".challenge").append("<button id='button-challenge'>Click me!</button>");
 
 // add a click listener to the challenge button
 $("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
+    $(".challenge").toggleClass("special");
+});
+
+$(".problems").append("<button id='button-problems'>Click me!</button>");
+
+// add a click listener to the problems button
+$("#button-problems").click(function(){
+    $(".problems").toggleClass("special");
+});
+
+$(".results").append("<button id='button-results'>Click me!</button>");
+
+// add a click listener to the results button
+$("#button-results").click(function(){
+    $(".results").toggleClass("special");
 });
