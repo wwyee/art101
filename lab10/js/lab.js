280bb2437ecc9f1fe00c6183bd0a0efe2380fdec
wwong32 <wwong32@ss1-mac215.lt.ucsc.edu>
2025-05-29 21:23:37 -0700

    lab14

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -19,10 +19,10 @@ function generateRandomText() {
 
 // click listener for button
 $("#make-convo").click(function(){
-});
 
 // get new fake dialogue
 const newText = generateRandomText();
 
 // append a new div to our output div
 $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
