2b2d2147e30defffd257627189521ec51639b098
wwong32 <wwong32@ss1-mac215.lt.ucsc.edu>
2025-05-29 22:39:48 -0700

    lab15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -2,27 +2,21 @@
 // Author: Marisa Wong
 // Date: 15 May 2025
 
-// Constants
+$('#getFortune').on('click', function () {
+  $.ajax({
+    url: "http://www.yerkee.com/api/fortune",
+    data: {},
+    type: "GET",
+    dataType: "json",
 
-// Functions
+    success: function (data) {
+      console.log(data);
+      $('#fortune').text(data.fortune);
+    },
 
-function generateRandomText() {
-  const text = "Praesent iaculis velit in lobortis sagittis. Fusce facilisis pretium enim sit amet accumsan. Duis nisi nulla, pellentesque ut blandit id, lacinia et sem. Sed sagittis leo eu erat posuere, ac pharetra diam condimentum. Nullam et nulla purus. Sed in turpis pharetra, lobortis erat sit amet, laoreet libero. Aliquam vel est nec felis efficitur vehicula et eget ligula. In hac habitasse platea dictumst. Sed sed tempus dolor, vitae dapibus libero.";
-  const min = 2;
-  const max = 100;
-  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-  // Get a random starting index to slice the Lorem Ipsum text
-  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-  // Generate the random Lorem Ipsum-like text
-  return text.slice(randStart, randStart + randLen);
-}
-
-// click listener for button
-$("#make-convo").click(function(){
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log("Error:", textStatus, errorThrown);
+      $('#fortune').text("Failed to load fortune.");
+    }
+  });
 });
-
-// get new fake dialogue
-const newText = generateRandomText();
-
-// append a new div to our output div
-$("#output").append('<div class="text"><p>' + newText + '</p></div>');
