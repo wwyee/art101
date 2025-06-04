24f38b11bc76b65bb9b3b9abd28197aab1fb7864
wwyee <114123370+wwyee@users.noreply.github.com>
2025-06-04 00:25:35 -0700

    Update lab.js

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,27 +2,26 @@
 // Author: Marisa Wong
 // Date: 15 May 2025
 
-// Constants
 
-// Functions
+// Using the core $.ajax() method
+$.ajax({
+  url: "https://xkcd.com/info.0.json",
+  data: {},
+  type: "GET",
+  dataType : "json",
+  success: function(comicObj) {
+      // do stuff
+      console.log(comicObj);
+      let title = data.title;
+      let num = data.num;
+      let alt = data.alt;
+      let imageUrl = data.img;
+  },
 
-function generateRandomText() {
-  const text = "Praesent iaculis velit in lobortis sagittis. Fusce facilisis pretium enim sit amet accumsan. Duis nisi nulla, pellentesque ut blandit id, lacinia et sem. Sed sagittis leo eu erat posuere, ac pharetra diam condimentum. Nullam et nulla purus. Sed in turpis pharetra, lobortis erat sit amet, laoreet libero. Aliquam vel est nec felis efficitur vehicula et eget ligula. In hac habitasse platea dictumst. Sed sed tempus dolor, vitae dapibus libero.";
-  const min = 2;
-  const max = 100;
-  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-  // Get a random starting index to slice the Lorem Ipsum text
-  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-  // Generate the random Lorem Ipsum-like text
-  return text.slice(randStart, randStart + randLen);
-}
 
-// click listener for button
-$("#make-convo").click(function(){
-});
-
-// get new fake dialogue
-const newText = generateRandomText();
-
-// append a new div to our output div
-$("#output").append('<div class="text"><p>' + newText + '</p></div>');
+  // What we do if the api call fails
+  error: function (jqXHR, textStatus, errorThrown) { 
+      // do stuff
+      console.log("Error loading XKCD comic:", textStatus, errorThrown);
+  }
+})
